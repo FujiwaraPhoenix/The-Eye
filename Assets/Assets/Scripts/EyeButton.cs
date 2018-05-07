74c182eecb2b9dad8dfa45b9a71eb9f5d8618bf7
Unknown <heianhourai@gmail.com>
2018-05-06 21:28:48 -0400

    Scene 4 modded, things in 3 fixed.

---
+++ Assets/Assets/Scripts/EyeButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeButton : MonoBehaviour {
+    public SpriteRenderer spr;
+    public Sprite normal, hovered;
+
+	// Use this for initialization
+	void Start () {
+        spr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        spr.sprite = hovered;
+    }
+
+    private void OnMouseExit()
+    {
+        spr.sprite = normal;
+    }
+}
